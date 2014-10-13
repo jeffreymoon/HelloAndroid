d118bf8119997ff27ac9bc59a03b27ec04d2276a
jeffreymoon <novmoon73@gmail.com>
2014-10-13 12:08:02 +0900

    init commit

---
+++ src/com/moonfactory/helloandroid/Sudoku.java
@@ -0,0 +1,58 @@
+package com.moonfactory.helloandroid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class Sudoku extends ActionBarActivity implements OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        View continueButton = this.findViewById(R.id.continue_button);
+        continueButton.setOnClickListener(this);
+        View newButton = this.findViewById(R.id.new_button);
+        newButton.setOnClickListener(this);
+        View aboutButton = this.findViewById(R.id.about_button);
+        aboutButton.setOnClickListener(this);
+        View exitButton = this.findViewById(R.id.exit_button);
+        exitButton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+        case R.id.about_button:
+            Intent i = new Intent(this, About.class);
+            startActivity(i);
+            break;
+        default:
+            break;
+        }
+    }
+    
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+        return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
