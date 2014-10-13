fdcc846422eca9f30dff38702ec15bdce75daf06
jeffreymoon <novmoon73@gmail.com>
2014-10-13 16:38:15 +0900

    menu is coding

---
+++ src/com/moonfactory/helloandroid/Settings.java
@@ -0,0 +1,13 @@
+package com.moonfactory.helloandroid;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class Settings extends PreferenceActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPrefeerencesFromResource(R.xml.settings);
+	}
+}
--- src/com/moonfactory/helloandroid/Sudoku.java
+++ src/com/moonfactory/helloandroid/Sudoku.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -41,19 +42,28 @@ public class Sudoku extends ActionBarActivity implements OnClickListener {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+    	super.onCreateOptionsMenu(menu);
+    	MenuInflater inflater = getMenuInflater();
+    	inflater.inflate(R.menu.menu, menu);
+//        getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+    	switch(item.getItemId()) {
+    	case R.id.settings:
+    		startActivity(new Intent(this, Settings.class));
+    		return true;
+    	}
+    	return false;
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-        return true;
-        }
-        return super.onOptionsItemSelected(item);
+//        int id = item.getItemId();
+//        if (id == R.id.action_settings) {
+//        return true;
+//        }
+//        return super.onOptionsItemSelected(item);
     }
 }
